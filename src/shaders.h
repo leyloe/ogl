de8fc058483efad15d87066ad5ff45c3958c5450
Your Name <your@email.com>
2026-05-24 17:14:02 -0700

    typo

--- src/shaders.h
+++ src/shaders.h
@@ -6,22 +6,22 @@
 static const char *vertex_shader_source = "#version 330 core\n"
                                           "layout (location = 0) in vec3 aPos;\n"
                                           "layout (location = 1) in vec2 aTexCoord;\n"
-                                          "out vec2 textCoord;\n"
+                                          "out vec2 texCoord;\n"
                                           "uniform mat4 model;\n"
                                           "uniform mat4 view;\n"
                                           "uniform mat4 proj;\n"
                                           "void main()\n"
                                           "{\n"
                                           "   gl_Position = proj * view * model * vec4(aPos, 1.0);\n"
-                                          "   textCoord = aTexCoord;\n"
+                                          "   texCoord = aTexCoord;\n"
                                           "}";
 static const char *fragment_shader_source = "#version 330 core\n"
-                                            "in vec2 textCoord;\n"
+                                            "in vec2 texCoord;\n"
                                             "uniform sampler2D texture0;\n"
                                             "out vec4 FragColor;\n"
                                             "void main()\n"
                                             "{\n"
-                                            "   FragColor = texture(texture0, textCoord);\n"
+                                            "   FragColor = texture(texture0, texCoord);\n"
                                             "}";
 
 #endif
